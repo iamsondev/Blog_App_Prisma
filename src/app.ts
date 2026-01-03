ff13c4bfd263bb746c535855a1de3a538a3e7e4f
iamsondev <sondipkumarsk@gmail.com>
2026-01-04 00:14:14 +0600

    added cors,email verification, email and password

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,16 @@ import express from "express";
 import { postRouter } from "./modules/post/post.router";
 import { auth } from "./lib/auth";
 import { toNodeHandler } from "better-auth/node";
+import cors from "cors";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: process.env.API_URL || "http://localhost:4000",
+    Credential: true,
+  })
+);
 
 app.all("/api/auth/*splat", toNodeHandler(auth));
 app.use(express.json());
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,4 @@
-import { betterAuth } from "better-auth";
+import { betterAuth, string } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
 
@@ -6,7 +6,33 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
+  trustedOrigins: [process.env.API_URL!],
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        defaultValue: "USER",
+        required: false,
+      },
+      phone: {
+        type: "string",
+        required: false,
+      },
+      status: {
+        type: "string",
+        defaultValue: "Active",
+        required: false,
+      },
+    },
+  },
   emailAndPassword: {
     enabled: true,
+    autoSignIn: false,
+    requireEmailVerification: true,
+  },
+  emailVerification: {
+    sendVerificationEmail: async ({ user, url, token }, request) => {
+      console.log("***verification email sent");
+    },
   },
 });
