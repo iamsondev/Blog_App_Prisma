48e26244107e7bbfac1e4ea90766c2907175fbfe
iamsondev <sondipkumarsk@gmail.com>
2026-01-05 16:58:29 +0600

    added middlewares

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,60 @@
+import { NextFunction, Request, Response } from "express";
+import { auth as betterAuth } from "../lib/auth";
+
+export enum useRole {
+  USER = "USER",
+  ADMIN = "ADMIN",
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        id: string;
+        email: string;
+        name: string;
+        role: string;
+        emailVerified: boolean;
+      };
+    }
+  }
+}
+const auth = (...roles: useRole[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const session = await betterAuth.api.getSession({
+        headers: req.headers as any,
+      });
+      if (!session) {
+        return res.status(401).json({
+          success: false,
+          message: "you are not authorized",
+        });
+      }
+      if (!session.user.emailVerified) {
+        return res.status(401).json({
+          success: false,
+          message: "email verification is must",
+        });
+      }
+      req.user = {
+        id: session.user.id,
+        email: session.user.email,
+        name: session.user.name,
+        role: session.user.role as string,
+        emailVerified: session.user.emailVerified,
+      };
+      if (roles.length && !roles.includes(req.user.role as useRole)) {
+        return res.status(403).json({
+          success: false,
+          message: "you don't have permission to access this resources",
+        });
+      }
+      next();
+    } catch (err) {
+      next(err);
+    }
+  };
+};
+
+export default auth;
--- src/modules/post/post.router.ts
+++ src/modules/post/post.router.ts
@@ -1,60 +1,8 @@
-import express, { NextFunction, Request, Response } from "express";
+import express from "express";
 import { postController } from "./post.controller";
-import { auth as betterAuth } from "../../lib/auth";
-import { success } from "better-auth/*";
+import auth, { useRole } from "../../middlewares/auth";
 const router = express.Router();
 
-export enum useRole {
-  USER = "USER",
-  ADMIN = "ADMIN",
-}
-
-declare global {
-  namespace Express {
-    interface Request {
-      user?: {
-        id: string;
-        email: string;
-        name: string;
-        role: string;
-        emailVerified: boolean;
-      };
-    }
-  }
-}
-const auth = (...roles: useRole[]) => {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    const session = await betterAuth.api.getSession({
-      headers: req.headers as any,
-    });
-    if (!session) {
-      return res.status(401).json({
-        success: false,
-        message: "you are not authorized",
-      });
-    }
-    if (!session.user.emailVerified) {
-      return res.status(401).json({
-        success: false,
-        message: "email verification is must",
-      });
-    }
-    req.user = {
-      id: session.user.id,
-      email: session.user.email,
-      name: session.user.name,
-      role: session.user.role as string,
-      emailVerified: session.user.emailVerified,
-    };
-    if (roles.length && !roles.includes(req.user.role as useRole)) {
-      return res.status(403).json({
-        success: false,
-        message: "you don't have permission to access this resources",
-      });
-    }
-    next();
-  };
-};
 router.post("/", auth(useRole.USER), postController.createPost);
 
 export const postRouter = router;
