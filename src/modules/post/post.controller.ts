81d4e1672d5d241512665ed5c11d73093939f00b
iamsondev <sondipkumarsk@gmail.com>
2026-01-06 16:47:04 +0600

    added seaerch option

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { postService } from "./post.service";
+import { string } from "better-auth/*";
 
 const createPost = async (req: Request, res: Response) => {
   try {
@@ -21,7 +22,9 @@ const createPost = async (req: Request, res: Response) => {
 
 const getAllPost = async (req: Request, res: Response) => {
   try {
-    const result = await postService.getAllPost();
+    const { search } = req.query;
+    const searchString = typeof search === "string" ? search : undefined;
+    const result = await postService.getAllPost({ search: searchString });
     res.status(200).json(result);
   } catch (e) {
     res.status(400).json({
